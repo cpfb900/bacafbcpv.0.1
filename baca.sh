950d3afee6867b5376bd4b4b15b8a193c45e8bb8
cpfb900 <74002115+cpfb900@users.noreply.github.com>
2020-11-12 09:01:40 +0700

    Add files via upload

---
+++ baca.sh
@@ -0,0 +1,58 @@
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 2
+echo
+toilet -f big -F gay Rizz Script-_
+echo
+echo
+echo $red"Selamat Datang Di FB CHECKPOINT REMOVER V.0.1 Beta"
+echo $yellow"Pembuat:Rizz Script-_"
+echo $yellow"Info:Mau Lebih Mudah ? Kami Open Bengkel Sesi cek dikomentar"
+echo $green"expired:15 Nov 2020"
+sleep 2
+echo $yellow"Script Yang Bertuliskan BETA Adalah Script ProtoType"
+echo $yellow"Server By:https://loadfbopencp.co.id"
+echo
+echo $white"1.) BUKA SESI BIASA"
+echo $red"2.) Buka Sesi Permanen FIX ERROR"
+echo $white"0.)Exit"
+echo $cyam
+read -p "PILIH MANA CUK: " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Sesi Biasa CP!
+echo $red"pastikan anda login dengan benar!"
+echo $white"Login To FB.."
+echo $cyan"Masukan Nomor Hp/Email:"
+read target
+echo $cyan"Masukan Password:"
+read target
+echo $white"Tunggu"
+sleep 3
+echo $red"coba cek "
+sleep 2
+echo $cyan"kalo ga work tunggu sc terbaru THX"
+exit
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay KAGA BISA LAGI ERROR GOBLOK!
+exit
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+exit
+fi
